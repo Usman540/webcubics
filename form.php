820e406d380218729a33a380b77a0af0165227c7
Usman540 <64222951+Usman540@users.noreply.github.com>
2023-07-23 02:19:47 +0500

    form.php

--- form.php
+++ form.php
@@ -1,1 +1,33 @@
 
+<?php
+// Retrieve form data
+$name = $_POST['fullName'];      
+$type = $_POST['type'];
+$contactInfo = $_POST['contactInfo'];
+
+// Connect to the database
+$servername = " localhost:3306";
+$username = "webcefjw_Usman540";
+$password = "AO69x#-=0uh^";
+$dbname = "webcefjw_PMContactFormDB";
+
+// Create a connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Insert data into the database
+$sql = "INSERT INTO your_table_name (fldFullName, fldType, fldCntInfo) VALUES ('$name', '$type', '$contactInfo')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Data inserted successfully.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the database connection
+$conn->close();
+?>
